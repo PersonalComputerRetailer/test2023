c8fb97ae04525fcc6fc316e9f48b63d21902fd7a
PersonalComputerRetailer <bill90128@gmail.com>
2024-03-25 22:10:09 -0700

    post6

--- tools/updateRSS.py
+++ tools/updateRSS.py
@@ -1,22 +1,22 @@
-import feedparser
 from bs4 import BeautifulSoup
-import requests
 from datetime import datetime
 
 # Function to extract relevant information from your website's HTML
-def extract_information(html_content):
-    soup = BeautifulSoup(html_content, 'html.parser')
-    articles = soup.find_all('li')  # Assuming each <li> contains an article
-    articles_info = []
-    for article in articles:
-        title = article.find('a').text.strip()
-        link = article.find('a')['href']
-        date_string = article.find('time')['datetime']
-        # Parse date string to datetime object
-        pubDate = datetime.strptime(date_string, "%Y-%m-%dT%H:%M:%S.%fZ").strftime("%a, %d %b %Y %H:%M:%S")
-        description = f"Published on {pubDate}"
-        articles_info.append({'title': title, 'link': link, 'description': description, 'pubDate': pubDate})
-    return articles_info
+def extract_information(file_path):
+    with open(file_path, 'r', encoding='utf-8') as f:
+        html_content = f.read()
+        soup = BeautifulSoup(html_content, 'html.parser')
+        articles = soup.find_all('li')  # Assuming each <li> contains an article
+        articles_info = []
+        for article in articles:
+            title = article.find('a').text.strip()
+            link = article.find('a')['href']
+            date_string = article.find('time')['datetime']
+            # Parse date string to datetime object
+            pubDate = datetime.strptime(date_string, "%Y-%m-%dT%H:%M:%S.%fZ").strftime("%a, %d %b %Y %H:%M:%S")
+            description = f"Published on {pubDate}"
+            articles_info.append({'title': title, 'link': link, 'description': description, 'pubDate': pubDate})
+        return articles_info
 
 # Function to generate RSS feed
 def generate_rss(articles_info):
@@ -40,15 +40,10 @@ def generate_rss(articles_info):
     rss_feed += '</rss>\n'
     return rss_feed
 
-# URL of your website
-website_url = 'https://willscyber.net/blog/blog.html'
-response = requests.get(website_url)
-if response.status_code == 200:
-    html_content = response.content
-    articles_info = extract_information(html_content)
-    rss_feed = generate_rss(articles_info)
-    with open('../blog/willscyber_blog_feed.xml', 'w', encoding='utf-8') as f:
+# local html
+html_file_path = '../blog/blog.html'
+articles_info = extract_information(html_file_path)
+rss_feed = generate_rss(articles_info)
+with open('../blog/willscyber_blog_feed.xml', 'w', encoding='utf-8') as f:
         f.write(rss_feed)
-    print('RSS feed generated successfully!')
-else:
-    print('Failed to retrieve website content.')
+print('RSS feed generated successfully!')
